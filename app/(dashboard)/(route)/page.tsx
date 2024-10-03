100096c841c1c1375ffb9328ac280d95b9d52e4e
ricky <ricky.madava@gmail.com>
2024-10-03 18:08:43 +0300

    Authentication with Clerk

--- app/(dashboard)/(route)/page.tsx
+++ app/(dashboard)/(route)/page.tsx
@@ -6,7 +6,9 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-2xl font-bold">Contenu principal</h1>
       <p>Cliquez sur l'icône de menu pour ouvrir/fermer la barre latérale.</p>
-      <p>This is a protected page</p>
+      <p className="text-2xl font-bold text-sky-500">
+        This is a protected page
+      </p>
     </div>
   );
 }
