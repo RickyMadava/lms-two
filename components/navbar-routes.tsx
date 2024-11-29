e5038b581fa697715a38bde18f3bc33f09b9cd0f
ricky <ricky.madava@gmail.com>
2024-11-29 10:38:29 +0300

    Search query 💫

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -5,39 +5,48 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
+import { SearchInput } from "./search-input";
 
 const NavbarRoutes = () => {
   const pathname = usePathname();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
   const isPlayerPage = pathname?.includes("/chapter");
+  const isSearchPage = pathname === "/search";
 
   return (
-    <div className="flex gap-x-2 ml-auto lg:mr-10">
-      {isTeacherPage || isPlayerPage ? (
-        <Link href="/">
-          <Button
-            size="sm"
-            variant="ghost"
-            className="transition-all duration-300 ease-in-out hover:bg-lime-600/20 hover:font-semibold"
-          >
-            <LogOut className="h-4 w-4 mr-2 " />
-            Exit
-          </Button>
-        </Link>
-      ) : (
-        <Link href="/teacher/courses">
-          <Button
-            size="sm"
-            variant="ghost"
-            className="transition-all duration-300 ease-in-out hover:bg-lime-600/20 hover:font-semibold"
-          >
-            Teacher mode
-          </Button>
-        </Link>
+    <>
+      {isSearchPage && (
+        <div className="hidden ml-10 md:block">
+          <SearchInput />
+        </div>
       )}
-      <UserButton />
-    </div>
+      <div className="flex gap-x-2 ml-auto lg:mr-10">
+        {isTeacherPage || isPlayerPage ? (
+          <Link href="/">
+            <Button
+              size="sm"
+              variant="ghost"
+              className="transition-all duration-300 ease-in-out hover:bg-lime-600/20 hover:font-semibold"
+            >
+              <LogOut className="h-4 w-4 mr-2 " />
+              Exit
+            </Button>
+          </Link>
+        ) : (
+          <Link href="/teacher/courses">
+            <Button
+              size="sm"
+              variant="ghost"
+              className="transition-all duration-300 ease-in-out hover:bg-lime-600/20 hover:font-semibold"
+            >
+              Teacher mode
+            </Button>
+          </Link>
+        )}
+        <UserButton />
+      </div>
+    </>
   );
 };
 
